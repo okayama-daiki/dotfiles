8b32c2ef0763dc0181a4425eacf1ee011e190ac4
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-01-25 19:55:28 +0900

    Configure some new settings

--- .bin/defaults.sh
+++ .bin/defaults.sh
@@ -14,7 +14,8 @@ defaults write com.apple.finder "QuitMenuItem" -int 1
 
 # Display -------------------------------------------------
 
-defaults write "com.apple.Terminal" "NSWindow Frame TTWindow Pro" -string "0 12 1690 1057 0 0 1710 1069 "
+# TODO: More Space Display, but not working
+# defaults write "com.apple.Terminal" "NSWindow Frame TTWindow Pro" -string "0 12 1690 1057 0 0 1710 1069"
 
 # Control Center ------------------------------------------
 
@@ -28,6 +29,8 @@ defaults write com.apple.dock orientation right
 defaults write com.apple.dock autohide -bool True
 # Size
 defaults write com.apple.dock tilesize -int 45
+# Animation
+defaults write com.apple.dock launchanim -int 0
 # Magnification
 defaults write com.apple.dock largesize -int 16
 killall Dock
@@ -52,8 +55,14 @@ defaults write "Apple Global Domain" "com.apple.trackpad.scrolling" -int 1
 # Natural scrolling
 defaults write "Apple Global Domain" "com.apple.swipescrolldirection" -int 0
 # Click weight
-defaults write "com.apple.AppleMultitouchTrackpad" "FirstClickThreshold" -int 1
-# Click weight
-defaults write "com.apple.AppleMultitouchTrackpad" "SecondClickThreshold" -int 1
+defaults write "com.apple.AppleMultitouchTrackpad" "FirstClickThreshold" -int 0
+defaults write "com.apple.AppleMultitouchTrackpad" "SecondClickThreshold" -int 0
 # Use trackpad for dragging (Tree Finger Drag)
 defaults write "com.apple.AppleMultitouchTrackpad" "TrackpadThreeFingerDrag" -int 1
+# Tap to click
+defualts write "com.apple.AppleMultitouchTrackpad" "Clicking" -int 1
+
+# Shortcuts -----------------------------------------------
+
+# Use function keys as standard function keys
+defaults write "Apple Global Domain" "com.apple.keyboard.fnState" -int 1
